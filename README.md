10a651be60854e7d365813f7495618b8dfe28a39
olerom <mefiader@gmail.com>
2017-03-08 02:03:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Formula One: history
+Application that allows you to find a historical record of F1 racing data.
+Using [Ergast Developer API](http://ergast.com/)
---
+++ console/src/main/java/com/olerom/formula/console/Application.java
@@ -0,0 +1,16 @@
+package com.olerom.formula.console;
+
+/**
+ * Date: 08.03.17
+ *
+ * @author olerom
+ */
+public class Application {
+    public static void main(String[] args) {
+        new Application().run();
+    }
+
+    private void run() {
+
+    }
+}
